dee4fb2608f3fca23cfdd912b037bc92714fecc8
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-02-25 20:33:07 -0500

    created favorite badge and coloring for author cards

--- pages/authors.js
+++ pages/authors.js
@@ -20,6 +20,7 @@ const showAuthors = (array) => {
       <div class="card-body">
         <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
+        <p class="card-text bold">${item.favorite ? '<span class="badge badge-info sale-badge" style="background-color:silver;"><i class="fa fa-star" aria-hidden="true" style="color:yellow;"></i> Favorite</span>' : ''}</p>
         <hr>
         <i class="btn btn-success fas fa-eye" id="view-author-btn--${item.firebaseKey}"></i>
         <i class="fas fa-edit btn btn-info" id="update-author--${item.firebaseKey}"></i>
